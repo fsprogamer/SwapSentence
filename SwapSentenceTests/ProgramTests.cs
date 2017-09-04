c516183cac676e380cf7a25b2c653fc2146a649c
fsprogamer <fsprogamer@gmail.com>
2017-09-04 17:15:12 +0300

    first commit

---
+++ SwapSentenceTests/ProgramTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SwapSentence.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void ReverseWords_empty_empty()        
+        {
+            string strIn = string.Empty;
+            string strExpected = string.Empty;
+            // act 
+            string strActual = Program.ReverseWords(strIn);
+            //assert
+            Assert.AreEqual(strExpected, strActual);
+        }
+    }
+}
---
+++ SwapWords/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SwapSentence
+{
+    public class Program
+    {
+        public static string ReverseWords(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+            Array.Reverse(words);
+            return string.Join(" ", words);
+        }
+        public static void ProcessFile()
+        {
+            string s;
+            int numberOfCases;
+
+            s = Console.ReadLine();
+            bool result = int.TryParse(s, out numberOfCases);
+            for (int i = 0; i <= numberOfCases; i++)
+            {
+                if ((s = Console.ReadLine()) != null)
+                {
+                    Console.WriteLine("Case #{0}: {1}", i + 1, ReverseWords(s));
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            ProcessFile();
+        }
+    }
+}
